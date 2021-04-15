d9a4af8a45c3e8bb938bde2a1e1eea4c9fc1c293
bahumu18 <43068085+bahumu18@users.noreply.github.com>
2021-04-15 11:28:46 +0200

    Update Script.js

--- Green Isac & Orchestra/GreenIsacOrchestra/Script.js
+++ Green Isac & Orchestra/GreenIsacOrchestra/Script.js
@@ -7,6 +7,24 @@
              document.getElementById("textbox").style.top = "0";
            
          }
+
+
+         //hamburger menu
+
+let menuToggler = document.querySelector(".nav__button");
+let navLinks = document.querySelectorAll(".nav-link");
+let body = document.querySelector("body");
+
+menuToggler.addEventListener("click", () => {
+    body.classList.toggle("open");
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        body.classList.toggle("open");
+    });
+});
+
        //slideshow
           var slideIndex = 0; 
                     showSlides();
@@ -68,7 +86,6 @@
 
 
 
-
   //Videos
   const state = {};
   const carouselList = document.querySelector(".carousel__list");
@@ -118,130 +135,24 @@
     return diff;
   }
 
-                       
-
+                      
 //Pictures
-var gallery = document.querySelector('.gallery');
-var galleryItems = document.querySelectorAll('.gallery-item');
-var numOfItems = gallery.children.length;
-var itemWidth = 23; // percent: as set in css
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  var lastned = document.getElementById("lastned");
+  lastned.href= imgs.src;
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
 
-var featured = document.querySelector('.featured-item');
+  expandImg.parentElement.style.display = "block";
 
-var rightBtn = document.querySelector('.move-btn.left');
-var leftBtn = document.querySelector('.move-btn.right');
-var leftInterval;
-var rightInterval;
-
-var scrollRate = 0.2;
-var left;
-if(document.getElementById("lukke").clicked == true)
-{
-   alert("button was clicked");
-   
+  document.getElementById("supercontainer").style.display="none";
 }
+function steng(){
+  //var expandImg = document.getElementById("expandedImg");
+  //expandImg.parentElement.style.display='none'
+  document.getElementById("supercontainer").style.display="block";
+  document.getElementById("container").style.display="none";
 
-function selectItem(e) {
-	if (e.target.classList.contains('active')) return;
-	
-	featured.style.backgroundImage = e.target.style.backgroundImage;
-	
-	for (var i = 0; i < galleryItems.length; i++) {
-		if (galleryItems[i].classList.contains('active'))
-			galleryItems[i].classList.remove('active');
-	}
-	document.getElementById("feature").style.display = 'block';
-	e.target.classList.add('active');
-    
-}
-function lukk(){
-    alert("button was clicked");
-}
-
-function galleryWrapLeft() {
-	var first = gallery.children[0];
-	gallery.removeChild(first);
-	gallery.style.left = -itemWidth + '%';
-	gallery.appendChild(first);
-	gallery.style.left = '0%';
-}
-
-function galleryWrapRight() {
-	var last = gallery.children[gallery.children.length - 1];
-	gallery.removeChild(last);
-	gallery.insertBefore(last, gallery.children[0]);
-	gallery.style.left = '-23%';
-}
-
-function moveLeft() {
-	left = left || 0;
-
-	leftInterval = setInterval(function() {
-		gallery.style.left = left + '%';
-
-		if (left > -itemWidth) {
-			left -= scrollRate;
-		} else {
-			left = 0;
-			galleryWrapLeft();
-		}
-	}, 1);
-}
-
-function moveRight() {
-	//Make sure there is element to the leftd
-	if (left > -itemWidth && left < 0) {
-		left = left  - itemWidth;
-		
-		var last = gallery.children[gallery.children.length - 1];
-		gallery.removeChild(last);
-		gallery.style.left = left + '%';
-		gallery.insertBefore(last, gallery.children[0]);	
-	}
-	
-	left = left || 0;
-
-	leftInterval = setInterval(function() {
-		gallery.style.left = left + '%';
-
-		if (left < 0) {
-			left += scrollRate;
-		} else {
-			left = -itemWidth;
-			galleryWrapRight();
-		}
-	}, 1);
-}
-
-function stopMovement() {
-	clearInterval(leftInterval);
-	clearInterval(rightInterval);
-}
-
-leftBtn.addEventListener('mouseenter', moveLeft);
-leftBtn.addEventListener('mouseleave', stopMovement);
-rightBtn.addEventListener('mouseenter', moveRight);
-rightBtn.addEventListener('mouseleave', stopMovement);
-
-
-//Start this baby up
-(function init() {
-	var images = [
-  'Images/victoria_med.jpeg',
-   'Images/Orchestra13.JPG',
-    'Images/Orchestra15.JPG',
-    'Images/GI 15.jpeg',
-    'Images/gio_med.jpg',
-      'Images/GIOband.jpg',
-     'Images/Orchestra14.JPG'
-	];
-	
-	//Set Initial Featured Image
-	featured.style.backgroundImage = 'url(' + images[0] + ')';
-	
-	//Set Images for Gallery and Add Event Listeners
-	for (var i = 0; i < galleryItems.length; i++) {
-		galleryItems[i].style.backgroundImage = 'url(' + images[i] + ')';
-		galleryItems[i].addEventListener('click', selectItem);
-	}
-})();
+} 
